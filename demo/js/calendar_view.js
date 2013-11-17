75ecae379b90efe2955786f66131cd90d77ad256
Peter Enns <slunk@umd.edu>
2013-11-17 13:03:02 -0500

    Sandboxed code a little better

--- demo/js/calendar_view.js
+++ demo/js/calendar_view.js
@@ -1,148 +1,145 @@
-// Using fake data for now
-// TODO: Figure out if there will be enough data that we should be using
-// LocalStorage
+(function () {
+    // Using fake data for now
+    // TODO: Figure out if there will be enough data that we should be using
+    // LocalStorage
 
-var routes = {
-    "Hethwood A": ["Burruss Hall", "Stroubles Circle"],
-    "Hethwood B": ["Torgersen Hall", "Tall Oaks"]
-};
+    fake_cal_data = [];
 
-var dt = new Date(2006, 0, 1),
-    data = [],
-    sched = 0,
-    actual = 0;
+    var routes = {
+        "Hethwood A": ["Burruss Hall", "Stroubles Circle"],
+        "Hethwood B": ["Torgersen Hall", "Tall Oaks"]
+    };
 
-for (var d = 0; d < 365 * 2; d++) {
-    for (var route in routes) {
-        sched = 0;
+    var dt = new Date(2006, 0, 1),
+        sched = 0,
         actual = 0;
-        for (var trip = 0; trip < 3; trip++) {
-            for (var stop = 0; stop < routes[route].length; stop++) {
-                sched += Math.floor(Math.random() * 50);
-                actual += Math.floor(Math.random() * 50);
-                data.push({
-                    "route": route,
-                    "date": dt,
-                    "trip": trip,
-                    "stop": routes[route][stop],
-                    "scheduled": sched,
-                    "actual": actual,
-                    "passengers": Math.floor(Math.random() * 30)
-                });
+
+    for (var d = 0; d < 365 * 2; d++) {
+        for (var route in routes) {
+            sched = 0;
+            actual = 0;
+            for (var trip = 0; trip < 3; trip++) {
+                for (var stop = 0; stop < routes[route].length; stop++) {
+                    sched += Math.floor(Math.random() * 50);
+                    actual += Math.floor(Math.random() * 50);
+                    fake_cal_data.push({
+                        "route": route,
+                        "date": dt,
+                        "trip": trip,
+                        "stop": routes[route][stop],
+                        "scheduled": sched,
+                        "actual": actual,
+                        "passengers": Math.floor(Math.random() * 30)
+                    });
+                }
             }
         }
-    }
-    dt = new Date(dt.getTime() + (24 * 60 * 60 * 1000));
-}
-
-// End of fake data generation
-
-var day = d3.time.format("%w"),
-    week = d3.time.format("%U"),
-    percent = d3.format(".1%"),
-    format = d3.time.format("%Y-%m-%d");
-
-var width = 960,
-    height = 136,
-    cellSize = 17; // cell size
-
-var displayCalendar = function(data, initSubview, subviewUpdate) {
-
-    if (!subviewUpdate) {
-        subviewUpdate = function () {};
+        dt = new Date(dt.getTime() + (24 * 60 * 60 * 1000));
     }
 
-    Date.prototype._cView_toISO = function () {
-        return this.toISOString().slice(0, 10);
-    }
+    // End of fake data generation
 
-var nested_data = d3.nest()
-    .key(function(d) { return d["date"]._cView_toISO(); })
-    .map(data);
+    var day = d3.time.format("%w"),
+        week = d3.time.format("%U"),
+        percent = d3.format(".1%"),
+        format = d3.time.format("%Y-%m-%d");
 
-var data_by_dow = d3.nest()
-    .key(function(d) { return day(d["date"]); })
-    .map(data);
+    var width = 960,
+        height = 136,
+        cellSize = 17; // cell size
 
-showDow = function (dow) {
-    updateTripView(data_by_dow["" + dow]);
-}
+    displayCalendar = function(data, subviewUpdate) {
+        if (!subviewUpdate) {
+            subviewUpdate = function () {};
+        }
 
-var adherence = function (d) {
-    var delta_sum = 0;
-    for (var i = 0; i < d.length; d++) {
-        delta_sum += Math.abs(d[i]["scheduled"] - d[i]["actual"]);
-    }
-    return delta_sum;
-};
+        Date.prototype._cView_toISO = function () {
+            return this.toISOString().slice(0, 10);
+        };
 
-var maxAdherence = d3.max(data, function (d) {
-    return adherence(nested_data[d["date"]._cView_toISO()]);
-});
+        var nested_data = d3.nest()
+            .key(function(d) { return d["date"]._cView_toISO(); })
+            .map(data);
 
-var color = d3.scale.quantize()
-    .domain([0.0, maxAdherence])
-    .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
+        var data_by_dow = d3.nest()
+            .key(function(d) { return day(d["date"]); })
+            .map(data);
 
-    var minYear = d3.min(data, function (d) { return +d.date.getUTCFullYear(); });
-    var maxYear = d3.max(data, function (d) { return +d.date.getUTCFullYear(); }) + 1;
+        showDow = function (dow) {
+            updateTripView(data_by_dow["" + dow]);
+        };
 
-var svg = d3.select("body").selectAll("svg")
-    .data(d3.range(minYear, maxYear))
-  .enter().append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("class", "RdYlGn")
-  .append("g")
-    .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+        var adherence = function (d) {
+            var delta_sum = 0;
+            for (var i = 0; i < d.length; d++) {
+                delta_sum += Math.abs(d[i]["scheduled"] - d[i]["actual"]);
+            }
+            return delta_sum;
+        };
 
-svg.append("text")
-    .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
-    .style("text-anchor", "middle")
-    .text(function(d) { return d; });
+        var maxAdherence = d3.max(data, function (d) {
+            return adherence(nested_data[d["date"]._cView_toISO()]);
+        });
 
-var rect = svg.selectAll(".day")
-    .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-  .enter().append("rect")
-    .attr("class", "day")
-    .attr("width", cellSize)
-    .attr("height", cellSize)
-    .attr("x", function(d) { return week(d) * cellSize; })
-    .attr("y", function(d) { return day(d) * cellSize; })
-    .datum(format)
-    //.on("mouseover", function (d) { console.log("Mouseover " + d); })
-    .on("click", function (d) { subviewUpdate(nested_data[d]); });
+        var color = d3.scale.quantize()
+            .domain([0.0, maxAdherence])
+            .range(d3.range(11).map(function(d) { return "q" + d + "-11"; }));
 
-rect.append("title")
-    .text(function(d) { return d; });
+            var minYear = d3.min(data, function (d) { return +d.date.getUTCFullYear(); });
+            var maxYear = d3.max(data, function (d) { return +d.date.getUTCFullYear(); }) + 1;
 
-svg.selectAll(".month")
-    .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
-  .enter().append("path")
-    .attr("class", "month")
-    .attr("d", monthPath);
+        var svg = d3.select("#calendar-container").selectAll("svg")
+            .data(d3.range(minYear, maxYear))
+          .enter().append("svg")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("class", "RdYlGn")
+          .append("g")
+            .attr("transform", "translate(" + ((width - cellSize * 53) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+        svg.append("text")
+            .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+            .style("text-anchor", "middle")
+            .text(function(d) { return d; });
+
+        var rect = svg.selectAll(".day")
+            .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+          .enter().append("rect")
+            .attr("class", "day")
+            .attr("width", cellSize)
+            .attr("height", cellSize)
+            .attr("x", function(d) { return week(d) * cellSize; })
+            .attr("y", function(d) { return day(d) * cellSize; })
+            .datum(format)
+            //.on("mouseover", function (d) { console.log("Mouseover " + d); })
+            .on("click", function (d) { subviewUpdate(nested_data[d]); });
+
+        rect.append("title")
+            .text(function(d) { return d; });
+
+        svg.selectAll(".month")
+            .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d + 1, 0, 1)); })
+          .enter().append("path")
+            .attr("class", "month")
+            .attr("d", monthPath);
 
 
-  rect.filter(function(d) { return d in nested_data; })
-      .attr("class", function(d) { return "day " + color(adherence(nested_data[d])); })
-    .select("title")
-    .text(function(d) { return d + ": " + percent(adherence(nested_data[d])); });
+          rect.filter(function(d) { return d in nested_data; })
+              .attr("class", function(d) { return "day " + color(adherence(nested_data[d])); })
+            .select("title")
+            .text(function(d) { return d + ": " + percent(adherence(nested_data[d])); });
+        };
 
-    if (initSubview) {
-        initSubview();
-    }
-};
+        function monthPath(t0) {
+          var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+              d0 = +day(t0), w0 = +week(t0),
+              d1 = +day(t1), w1 = +week(t1);
+          return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize +
+              "H" + w0 * cellSize + "V" + 7 * cellSize +
+              "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize +
+              "H" + (w1 + 1) * cellSize + "V" + 0 +
+              "H" + (w0 + 1) * cellSize + "Z";
+        }
 
-
-function monthPath(t0) {
-  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
-      d0 = +day(t0), w0 = +week(t0),
-      d1 = +day(t1), w1 = +week(t1);
-  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
-      + "H" + w0 * cellSize + "V" + 7 * cellSize
-      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
-      + "H" + (w1 + 1) * cellSize + "V" + 0
-      + "H" + (w0 + 1) * cellSize + "Z";
-}
-
-d3.select(self.frameElement).style("height", "2910px");
+        d3.select(self.frameElement).style("height", "2910px");
+})();
--- demo/js/trip_view.js
+++ demo/js/trip_view.js
@@ -1,182 +1,186 @@
-function truncate(str, maxLength, suffix) {
-	if(str.length > maxLength) {
-		str = str.substring(0, maxLength + 1); 
-		str = str.substring(0, Math.min(str.length, str.lastIndexOf(" ")));
-		str = str + suffix;
-	}
-	return str;
-}
-
-/* Using fake data for now */
-
-var stops = ["Squires West", "Main Red / Maple Nbnd", "Squires East", "Fairfax Ellett Nbnd"];
-var fake_data = [];
-
-for (var i = 0; i < 10; i++) {
-    var time = 0,
-        actual = 0;
-    for (var j = 0; j < stops.length; j++) {
-        time += Math.floor(Math.random() * 100)
-        actual += Math.floor(Math.random()  * 100);
-        fake_data.push({
-                    "trip": i,
-                    "stop": stops[j],
-                    "scheduled": time,
-                    "actual": actual,
-                    "passengers": Math.floor(Math.random() * 50)
-                });
+(function () {
+    function truncate(str, maxLength, suffix) {
+        if(str.length > maxLength) {
+            str = str.substring(0, maxLength + 1);
+            str = str.substring(0, Math.min(str.length, str.lastIndexOf(" ")));
+            str = str + suffix;
+        }
+        return str;
     }
-}
 
-/* End fake data generation */
+    /* Using fake data for now */
 
-var xAxis;
-var initTripView = function () {
-margin = {top: 20, right: 200, bottom: 0, left: 20};
-	width = 300;
-	height = 650;
+    var stops = ["Squires West", "Main Red / Maple Nbnd", "Squires East", "Fairfax Ellett Nbnd"];
+    fake_data = [];
 
-svg = d3.select("body").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.style("margin-left", margin.left + "px")
-	.append("g")
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-}
-
-var updateTripView = function (data) {
-var c = d3.scale.category20c();
-
-var x = d3.scale.linear()
-	.range([0, width]);
-
-    var stops = [];
-    for (var i = 0; i < data.length; i++) {
-        if (stops.indexOf(data[i]["stop"]) == -1) {
-            stops.push(data[i]["stop"]);
+    for (var i = 0; i < 10; i++) {
+        var time = 0,
+            actual = 0;
+        for (var j = 0; j < stops.length; j++) {
+            time += Math.floor(Math.random() * 100);
+            actual += Math.floor(Math.random()  * 100);
+            fake_data.push({
+                        "trip": i,
+                        "stop": stops[j],
+                        "scheduled": time,
+                        "actual": actual,
+                        "passengers": Math.floor(Math.random() * 50)
+                    });
         }
     }
 
-    var maxPassengers = d3.max(data, function(d) { return d.passengers; });
-    var maxAdherence = d3.max(data, function(d) { return Math.abs(d.scheduled - d.actual); });
+    /* End fake data generation */
 
-	var rScale = d3.scale.linear()
-		.domain([0, maxPassengers])
-		.range([2, 9]);
+    var xAxis,
+        margin = {top: 20, right: 200, bottom: 0, left: 20},
+        width = 300,
+        height = 650;
 
-    x = d3.scale.ordinal()
-        .domain(stops)
-        .range(_.range(stops.length));
+    var svg = d3.select("#trip-container").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("margin-left", margin.left + "px")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("top");
+    updateTripView = function (data) {
+        var c = d3.scale.category20c();
 
-    d3.selectAll(".journal").remove();
+        var x = d3.scale.linear()
+            .range([0, width]);
 
-    var data = d3.nest()
-        .key(function (d) { return d["trip"]; })
-        .key(function (d) { return d["stop"]; })
-        .map(data);
+        var stops = [];
+        for (var i = 0; i < data.length; i++) {
+            if (stops.indexOf(data[i]["stop"]) == -1) {
+                stops.push(data[i]["stop"]);
+            }
+        }
 
-    /*data = d3.nest()
-        .key(function (d) { return d["trip"]; })
-        .map(data);*/
+        var maxPassengers = d3.max(data, function(d) { return d.passengers; });
+        var maxAdherence = d3.max(data, function(d) { return Math.abs(d.scheduled - d.actual); });
 
-	var xScale = d3.scale.linear()
-		.domain([x(stops[0]), x(stops[stops.length - 1])])
-		.range([0, width]);
+        var rScale = d3.scale.linear()
+            .domain([0, maxPassengers])
+            .range([2, 9]);
 
-    var abbr = function (str) {
-        var words = str.split(' ');
-        return words.map(function (word) { return word[0]; }).join('');
-    }
-    console.log(data);
-    console.log(stops);
+        x = d3.scale.ordinal()
+            .domain(stops)
+            .range(_.range(stops.length));
 
-	var g = svg.append("g").attr("class","journal");
-	var text = g.selectAll("text")
-		.data(stops)
-		.enter()
-		.append("text");
-	text
-		.attr("y", 0)
-		.attr("x",function(d, i) { return 50 + xScale(x(d))-5; })
-		.attr("class","value")
-        .style("fill", function () { return d3.hsl("#4A4A4A"); })
-		.text(function(d){ return abbr(d); });
+        xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("top");
 
-	for (trip_no in data) {
-        var trip = Object.keys(data[trip_no]).map(function (key) {
-            return {"stop": key,
-                "entries": data[trip_no][key]
-            };
-        });
-		var g = svg.append("g").attr("class","journal");
+        d3.selectAll(".journal").remove();
 
-		var circles = g.selectAll("circle")
-			.data(trip)
-			.enter()
-			.append("circle");
+        data = d3.nest()
+            .key(function (d) { return d["trip"]; })
+            .key(function (d) { return d["stop"]; })
+            .map(data);
 
-		var text = g.selectAll("text")
-			.data(trip)
-			.enter()
-			.append("text");
+        var xScale = d3.scale.linear()
+            .domain([x(stops[0]), x(stops[stops.length - 1])])
+            .range([0, width]);
+
+        var abbr = function (str) {
+            var words = str.split(' ');
+            return words.map(function (word) { return word[0]; }).join('');
+        };
+
+        var g = svg.append("g").attr("class","journal");
+        var text = g.selectAll("text")
+            .data(stops)
+            .enter()
+            .append("text");
+
+        text
+            .attr("y", 0)
+            .attr("x",function(d, i) { return 50 + xScale(x(d))-5; })
+            .attr("class","value")
+            .style("fill", function () { return d3.hsl("#4A4A4A"); })
+            .text(function(d){ return abbr(d); });
+
+        var stopList = function (obj) {
+            return Object.keys(obj).map(function (key) {
+                return {"stop": key,
+                    "entries": obj[key]
+                };
+                });
+        };
 
         var avePassengers = function (d) {
             return d.entries.reduce(function (acc, curr) {
                 return acc + curr.passengers;
             }, 0) / d.entries.length;
-        }
+        };
 
         var aveAdherence = function (d) {
             return d.entries.reduce(function (prev, curr) {
                 return prev + Math.abs(curr.scheduled - curr.actual);
             }, 0) / d.entries.length;
-        }
+        };
 
         var circleFill = function (d) {
-			return d3.hsl(0, aveAdherence(d) / maxAdherence, 0.7);
-        }
+            return d3.hsl(0, aveAdherence(d) / maxAdherence, 0.7);
+        };
 
         var circleRadius = function (d) {
             return rScale(avePassengers(d));
+        };
+
+        var circleX = function (d) {
+            return 50 + xScale(x(d.stop));
+        };
+
+        for (var trip_no in data) {
+            var trip = stopList(data[trip_no]);
+
+            g = svg.append("g").attr("class","journal");
+
+            var circles = g.selectAll("circle")
+                .data(trip)
+                .enter()
+                .append("circle");
+
+            text = g.selectAll("text")
+                .data(trip)
+                .enter()
+                .append("text");
+
+            circles
+                .attr("cx", circleX)
+                .attr("cy", trip_no*20+20)
+                .attr("r", circleRadius)
+                .style("fill", circleFill);
+
+            text
+                .attr("y", trip_no*20+25)
+                .attr("x",function(d, i) { return 50 + xScale(x(d.stop))-5; })
+                .attr("class","value")
+                .text(function(d){ return ("" + avePassengers(d)).slice(0,5); })
+                .style("fill", circleFill)
+                .style("display","none");
+
+            g.append("text")
+                .attr("y", trip_no*20+25)
+                .attr("x", 0)
+                .attr("class","label")
+                .text(truncate("Trip " + trip_no,30,"..."))
+                .style("fill", function(d) { return d3.hsl("#4A4A4A"); })
+                .on("mouseover", mouseover)
+                .on("mouseout", mouseout);
         }
 
-		circles
-			.attr("cx", function(d, i) { return 50 + xScale(x(d.stop)); })
-			.attr("cy", trip_no*20+20)
-			.attr("r", circleRadius)
-			.style("fill", circleFill);
+        function mouseover(p) {
+            var g = d3.select(this).node().parentNode;
+            d3.select(g).selectAll("circle").style("display","none");
+            d3.select(g).selectAll("text.value").style("display","block");
+        }
 
-		text
-			.attr("y", trip_no*20+25)
-			.attr("x",function(d, i) { return 50 + xScale(x(d.stop))-5; })
-			.attr("class","value")
-			.text(function(d){ return d.passengers; })
-			.style("fill", circleFill)
-			.style("display","none");
-
-		g.append("text")
-			.attr("y", trip_no*20+25)
-			.attr("x", 0)
-			.attr("class","label")
-			.text(truncate("Trip " + trip_no,30,"..."))
-			.style("fill", function(d) { return d3.hsl("#4A4A4A"); })
-			.on("mouseover", mouseover)
-			.on("mouseout", mouseout);
-	};
-
-	function mouseover(p) {
-		var g = d3.select(this).node().parentNode;
-		d3.select(g).selectAll("circle").style("display","none");
-		d3.select(g).selectAll("text.value").style("display","block");
-	}
-
-	function mouseout(p) {
-		var g = d3.select(this).node().parentNode;
-		d3.select(g).selectAll("circle").style("display","block");
-		d3.select(g).selectAll("text.value").style("display","none");
-	}
-};
+        function mouseout(p) {
+            var g = d3.select(this).node().parentNode;
+            d3.select(g).selectAll("circle").style("display","block");
+            d3.select(g).selectAll("text.value").style("display","none");
+        }
+    };
+})();
