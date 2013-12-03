5340628fea90062b231cff9ed852a23693071844
superhans <varun.manjunatha@gmail.com>
2013-12-03 00:12:01 -0500

    Reduced Gaps

--- app/js/route_view.js
+++ app/js/route_view.js
@@ -16,13 +16,13 @@ var c = d3.scale.ordinal()
 var x = d3.scale.linear()
     .range([0, width]);
 
-var xAxis = d3.svg.axis()
+/*var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top")
-    .ticks(last_stop);
+    .ticks(last_stop);*/
 
 var formatYears = d3.format("00");
-xAxis.tickFormat(formatYears);
+//xAxis.tickFormat(formatYears);
 
 var svg = d3.select("#route_id").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -43,10 +43,10 @@ d3.json("./data/routes3.json", function(data) {
         .range([0, width]);
 
 
-    var xAxisG = svg.append("g")
+  /*  var xAxisG = svg.append("g")
         .attr("class", "x axis")
         .attr("transform", "translate(0," + 0 + ")")
-        .call(xAxis);
+        .call(xAxis);*/
 
     var nstops = [];
     for (var k = 0; k < data.length; k++) {
@@ -55,10 +55,13 @@ d3.json("./data/routes3.json", function(data) {
         //nstops[k] = data[k]['total'];
     }
 
+    x_gap = 10;
+    y_gap = 25;
+    y_offset = 5;
 
     for (var j = 0; j < data.length; j++) {
 
-    	console.log(data[j].name);
+    	//console.log(data[j].name);
         var g = svg.append("g").attr("class", "journal");
 
         var circles = g.selectAll("circle")
@@ -73,14 +76,14 @@ d3.json("./data/routes3.json", function(data) {
 
         var xAxisScale = d3.scale.linear()
             .domain([0, nstops[j]])
-            .range([0, nstops[j] * 23.07]);
+            .range([0, nstops[j] * x_gap]);
 
 
         circles
             .attr("cx", function(d, i) {
                 return xAxisScale(i);
             })
-            .attr("cy", j * 40 + 25)
+            .attr("cy", j * y_gap + y_offset)
             .attr("r", 5)
             .style("fill", function(d) {
                 return "#6cb3f8";
@@ -93,8 +96,9 @@ d3.json("./data/routes3.json", function(data) {
 
 
         g.append("text")
-            .attr("y", j * 40 + 25)
-            .attr("x", width + 20)
+            .attr("y", j * y_gap + y_offset + 3)
+            //.attr("x", width - 100)
+            .attr("x", x_gap*data[j].routes.length)
             .attr("class", "label")
             .text(data[j]['name'])
             .style("fill", function(d) {
@@ -156,7 +160,7 @@ d3.json("./data/routes3.json", function(data) {
             }
         }).style("fill", function(d, i) {
             if (d == stop_name) {
-                return "#6cb3f8";
+                return "red";
             } else {
                 return "#6cb3f8";
             }
@@ -165,26 +169,25 @@ d3.json("./data/routes3.json", function(data) {
 
     function circle_mouseover() {
 
-
-    	d3.select(this).style("fill", "red").attr("stroke-width", 1).attr("r", "10");
+    	d3.select(this).style("fill", "red").attr("stroke-width", 1).attr("r", "5");
         var xpos = d3.select(this).attr("cx");
         var ypos = d3.select(this).attr("cy");
-        xindex = Math.round(xpos / 23.07); //these numbers are really important
-        yindex = Math.round((ypos - 25) / 40);
+        xindex = Math.round(xpos / x_gap); //these numbers are really important
+        yindex = Math.round((ypos - y_offset) / y_gap);
         stop_name = data[yindex].routes[xindex].name;
        // console.log(stop_name);
         tool_tip_x = xpos;
         tool_tip_y = ypos-10;
 
 
-        xAxisG.selectAll('.tick')
+      /*  xAxisG.selectAll('.tick')
         .each(function(d,i){
         	if(d==(xindex+1)){
         		d3.select(this)
         			.selectAll('text')
         			.style("fill","red");
         	}
-        })
+        })*/
 
 		//Create the tooltip label
         svg.append("text")
@@ -196,12 +199,12 @@ d3.json("./data/routes3.json", function(data) {
             .attr("font-size", "11px")
             .attr("font-weight", "bold")
             .attr("fill", "black")
-            .text(stop_name);
+            .text("#"+(xindex+1)+" "+stop_name);
 
 
 
   //       console.log(stop_name);
-	//	map_highlightStops([stop_name]);
+		map_highlightStops([stop_name]);
   //       console.log("after");
 
         var map_height = parseInt(d3.select("#map_id").style("height"));
@@ -220,7 +223,7 @@ d3.json("./data/routes3.json", function(data) {
             }
         ).attr("r", function(d, i) {
             if (d.name == stop_name) {
-                return 10;
+                return 5;
             } else {
                 return 5;
             }
@@ -243,12 +246,12 @@ d3.json("./data/routes3.json", function(data) {
         var xpos = d3.select(this).attr("cx");
         var ypos = d3.select(this).attr("cy");
 
-        xindex = Math.round(xpos / 23.07); //these numbers are really important
-        yindex = Math.round((ypos - 25) / 40);
+        xindex = Math.round(xpos / x_gap); //these numbers are really important
+        yindex = Math.round((ypos - y_offset) / y_gap);
         stop_name = data[yindex].routes[xindex].name;
         //map_unhighlightStops([stop_name]);
 
-		xAxisG.selectAll('.tick')
+		/*xAxisG.selectAll('.tick')
         .each(function(d,i){
         	//console.log(xindex);
         	if(d==(xindex+1)){
@@ -256,7 +259,7 @@ d3.json("./data/routes3.json", function(data) {
         			.selectAll('text')
         			.style("fill","gray");
         	}
-        })
+        })*/
 
         d3.selectAll("#route_id circle").attr("stroke-width", 1).style("fill", "#6cb3f8").attr("r", "5");
         //d3.select("text").text(null);
@@ -266,33 +269,35 @@ d3.json("./data/routes3.json", function(data) {
     function mouseover() {
         d3.selectAll("#route_id circle").attr("stroke-width", 1).attr("r", "5");
         var g = d3.select(this).node().parentNode;
-        d3.select(g).selectAll("circle").attr("stroke-width", 1).attr("r", "10");
+        d3.select(g).selectAll("circle").attr("stroke-width", 1).attr("r", "5").style("fill","red");
         var ypos = d3.select(this).attr("y");
-        yindex = Math.round((ypos - 25) / 40);
+        yindex = Math.round((ypos - y_offset) / y_gap);
         //console.log(yindex);
         //console.log(data[yindex].routes);
         stop_names = [];
         for(var i=0;i<data[yindex].routes.length;i++){
         	stop_names[i] = data[yindex].routes[i].name;
         }
-        //map_highlightStopsCircleOnly(stop_names);
 
         d3.select(this).style("font-weight", "bold");
         d3.select(this).style("text-decoration", "underline");
+
+        map_highlightStopsCircleOnly(stop_names);
     }
 
     function mouseout() {
         var g = d3.select(this).node().parentNode;
         d3.select(g).selectAll("circle").transition().attr("stroke-width", 1).attr("r", "5");
+        d3.select(g).selectAll("circle").attr("stroke-width", 1).attr("r", "5").style("fill","#6cb3f8");
         var ypos = d3.select(this).attr("y");
-        yindex = Math.round((ypos - 25) / 40);
+        yindex = Math.round((ypos - y_offset) / y_gap);
         //console.log(this);
         stop_names = [];
         for(var i=0;i<data[yindex].routes.length;i++){
         	stop_names[i] = data[yindex].routes[i].name;
         }
         //map_unhighlightStops(data[yindex].routes);
-        //map_unhighlightStops(stop_names);
+        map_unhighlightStops(stop_names);
 
         d3.select(this).style("font-weight", "normal");
         d3.select(this).style("text-decoration", "none");
@@ -300,7 +305,7 @@ d3.json("./data/routes3.json", function(data) {
 
     function textmouseclick() {
         var ypos = d3.mouse(this)[1];
-        yindex = Math.round((ypos - 25) / 40);
+        yindex = Math.round((ypos - y_offset) / y_gap);
         //alert("" + data[yindex].name);
         var g = d3.select(this).node().parentNode;
         d3.select(g).selectAll("circle").attr("stroke-width", 1);
@@ -317,8 +322,8 @@ d3.json("./data/routes3.json", function(data) {
     function circle_mouseclick() {
         var xpos = d3.mouse(this)[0];
         var ypos = d3.mouse(this)[1];
-        xindex = Math.round(xpos / 23.07); //these numbers are really important
-        yindex = Math.round((ypos - 25) / 40);
+        xindex = Math.round(xpos / x_gap); //these numbers are really important
+        yindex = Math.round((ypos - y_offset) / y_gap);
         var selected_stop_ID = data[yindex].routes[xindex].stopId;
         //console.log(selected_stop_ID);
         switchToStopView();
